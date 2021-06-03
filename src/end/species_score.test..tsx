37305b908d401249b800c2247a3089df2e20eddf
Pedro Martins <pedrovrima@gmail.com>
2021-06-03 09:34:45 -0300

    species score

---
+++ src/end/species_score.test..tsx
@@ -0,0 +1,29 @@
+import SpeciesScoreGroup from "./progress";
+import { render, screen } from "@testing-library/react";
+
+const spp = [
+  {
+    genus: "a",
+    species: "b",
+    pt_name: "c",
+    old_score: 10,
+    to_sum: 5,
+    img_url: "string;",
+  },
+  {
+    genus: "d",
+    species: "e",
+    pt_name: "f",
+    old_score: 10,
+    to_sum: 5,
+    img_url: "string;",
+  },
+];
+
+test("options", async () => {
+  render(<SpeciesScoreGroup spp={spp} />);
+  expect(screen.getByText("a b")).toBeInTheDocument();
+
+  expect(screen.getByText("c")).toBeInTheDocument();
+  expect(screen.getByText("d e")).toBeInTheDocument();
+});
--- src/end/species_score.tsx
+++ src/end/species_score.tsx
@@ -1,27 +1,41 @@
-
 type speciesScore = {
-    genus:string
-    species:string
-    pt_name:string
-    old_score:number
-    to_sum:number
-    img_url:string
-}
+  genus: string;
+  species: string;
+  pt_name: string;
+  old_score: number;
+  to_sum: number;
+  img_url: string;
+};
 
-type speciesScoreProps ={
-    spp: speciesScore[]
-}
+type speciesScoreProps = {
+  spp: speciesScore[];
+};
 
-const SpeciesScore = (props:speciesScoreProps)=>{
-    const {spp} = props
+const SpeciesScore = (props: speciesScore) => {
+  const { genus, species, pt_name, old_score, to_sum, img_url } = props;
+  return (
+    <div key={pt_name}>
+      <h1>{pt_name}</h1>
+      <h2>{`${genus} ${species}`}</h2>
+      <p>{old_score + to_sum}</p>
+    </div>
+  );
+};
 
-    return(
-        <div>
-        spp
+const SpeciesScoreGroup = (props: speciesScoreProps) => {
+  const { spp } = props;
 
-        </div>
-    )
-}
+  return (
+    <div>
+      {spp.map((species: speciesScore) => {
+        return (
+          <div>
+            <SpeciesScore {...species}></SpeciesScore>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-
-export default SpeciesScore
+export default SpeciesScoreGroup;
